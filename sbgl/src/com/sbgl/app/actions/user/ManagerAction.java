36501398889b703ee1c45d8beb517b129e48e843
Beleege <ele52099@qq.com>
2014-01-14 21:44:35 +0800

    管理员管理功能全部实现

--- sbgl/src/com/sbgl/app/actions/user/ManagerAction.java
+++ sbgl/src/com/sbgl/app/actions/user/ManagerAction.java
@@ -117,12 +117,14 @@ public class ManagerAction extends ActionSupport implements SessionAware {
 			this.tag = "1";
 			this.message = "修改管理员信息失败！";
 		} else {
-			Usergrouprelation ugr = userGroupRelationService.getRelationByType(manager.getId(), 0);
+			Usergrouprelation ugr = userGroupRelationService.getRelationByType(manager.getId(), 8);
 			ugr.setGroupId( group.getId() );
 			userGroupRelationService.alterUserGroupRelation( ugr );
 			this.tag = "0";
 			this.message = "修改管理员信息成功！";
 		}
+		
+		gotoUserManageAdmin();
 		
 		returnJSON.put("tag", tag);
 		returnJSON.put("msg", message);
@@ -148,7 +150,7 @@ public class ManagerAction extends ActionSupport implements SessionAware {
 		for (String id : ids) {
 			Integer oneId = Integer.valueOf( id );
 			Administrator mg = managerService.getManagerById( oneId );
-			Usergrouprelation temp = userGroupRelationService.getRelationByType(mg.getId(), 0);
+			Usergrouprelation temp = userGroupRelationService.getRelationByType(mg.getId(), 8);
 			if(managerService.deleteManager( oneId ) == 0 && userGroupRelationService.deleteUserGroupRelation( temp.getId() ) == 0) {
 				this.message = "删除管理员信息成功！";
 				this.tag = "0";
@@ -157,6 +159,8 @@ public class ManagerAction extends ActionSupport implements SessionAware {
 				this.tag = "1";
 			}
 		}
+		
+		gotoUserManageAdmin();
 		
 		returnJSON.put("tag", tag);
 		returnJSON.put("msg", message);
