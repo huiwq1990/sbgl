63a9160bf9ff45516bfbedf29213c74e8529c385
Beleege <ele52099@qq.com>
2014-03-05 21:03:55 +0800

    添加新的用户Service层

---
+++ sbgl/src/com/sbgl/app/actions/user/UserAction.java
@@ -0,0 +1,22 @@
+package com.sbgl.app.actions.user;
+
+import java.util.Map;
+
+import org.apache.struts2.interceptor.SessionAware;
+
+import com.opensymphony.xwork2.ActionSupport;
+
+public class UserAction extends ActionSupport implements SessionAware {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = -3856779529100525958L;
+
+	@Override
+	public void setSession(Map<String, Object> arg0) {
+		// TODO Auto-generated method stub
+		
+	}
+	
+}
