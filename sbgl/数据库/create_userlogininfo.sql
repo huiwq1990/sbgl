54e777faa915683d3d1ad4146e3f295c684e7e46
Beleege <ele52099@qq.com>
2014-07-30 21:28:43 +0800

    用户登录信息添加界面语言信息

--- sbgl/数据库/create_userlogininfo.sql
+++ sbgl/数据库/create_userlogininfo.sql
@@ -1,3 +1,8 @@
+drop table if exists userlogininfo;
+
+/*==============================================================*/
+/* Table: userlogininfo                                         */
+/*==============================================================*/
 create table userlogininfo
 (
    id                   int not null,
@@ -5,6 +10,7 @@ create table userlogininfo
    isfirstlogin         varchar(5),
    logincount           int,
    lastlogintime        datetime,
+   pagelanguage         varchar(5),
    remark               varchar(100),
    primary key (id)
 );
