0e28958e3c3f61e3529ce3f684c851ce6545d8fe
Beleege <ele52099@qq.com>
2014-08-30 21:25:51 +0800

    给teacher表和worker表添加可否借阅权限项，并提供修改表结构sql语句

---
+++ sbgl/数据库/alter_teacher_and_worker.sql
@@ -0,0 +1,3 @@
+ALTER TABLE `sbgl`.`teacher` ADD COLUMN `couldborrow` VARCHAR(1) NULL  AFTER `modifydate` ;
+
+ALTER TABLE `sbgl`.`worker` ADD COLUMN `couldborrow` VARCHAR(1) NULL  AFTER `modifydate` ;
